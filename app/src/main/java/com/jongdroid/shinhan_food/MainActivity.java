52bce7f89fdaa907b103c8f4305d3e99fd0ac80b
hoonjong <qkr7627@naver.com>
2020-12-10 23:19:25 +0900

    첫번째 커밋입니다. -2020.12.10 신한대식당 시작
    -데이트피커까지 완성(XML)
    
    -MainActivity.java 데이트피커 날짜 조정시 토스트 메시지 까지 - (완료)

--- app/src/main/java/com/jongdroid/shinhan_food/MainActivity.java
+++ app/src/main/java/com/jongdroid/shinhan_food/MainActivity.java
@@ -15,14 +15,39 @@ import java.util.Calendar;
 
 public class MainActivity extends Activity {
 
-
+    DatePicker mDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mDate = findViewById(R.id.datePicker);
 
 
+        mDate.init(mDate.getYear(), mDate.getMonth(), mDate.getDayOfMonth(),
+
+                new DatePicker.OnDateChangedListener() {
+
+                    //값이 바뀔때마다 텍스트뷰의 값을 바꿔준다.
+
+                    @Override
+
+                    public void onDateChanged(DatePicker view, int year, int monthOfYear,
+
+                                              int dayOfMonth) {
+
+                        // TODO Auto-generated method stub
+
+
+                        //monthOfYear는 0값이 1월을 뜻하므로 1을 더해줌 나머지는 같다.
+
+                        Toast.makeText(MainActivity.this, "테스트입니다" ,Toast.LENGTH_LONG).show();
+
+
+
+                    }
+
+                });
     }
 }
