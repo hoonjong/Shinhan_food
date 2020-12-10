1ed986ac3c40fd8722edec039f953d3d35591146
hoonjong <qkr7627@naver.com>
2020-12-10 23:04:27 +0900

    첫번째 커밋입니다. -2020.12.10 신한대식당 시작
    -데이트피커까지 완성(XML)

--- app/src/main/java/com/jongdroid/shinhan_food/MainActivity.java
+++ app/src/main/java/com/jongdroid/shinhan_food/MainActivity.java
@@ -1,14 +1,28 @@
 package com.jongdroid.shinhan_food;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.os.Bundle;
-//안녕하세요
-public class MainActivity extends AppCompatActivity {
+import android.app.Activity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.app.DialogFragment;
+import android.widget.Toast;
+
+import java.util.Calendar;
+
+public class MainActivity extends Activity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
     }
 }
