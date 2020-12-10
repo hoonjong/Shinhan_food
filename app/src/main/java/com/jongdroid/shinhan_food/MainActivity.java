f6a271b662748fa4bc03467a8b10762143341de9
hoonjong <qkr7627@naver.com>
2020-12-10 21:49:35 +0900

    첫번째 커밋입니다. -2020.12.10 신한대식당 시작

---
+++ app/src/main/java/com/jongdroid/shinhan_food/MainActivity.java
@@ -0,0 +1,14 @@
+package com.jongdroid.shinhan_food;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+//안녕하세요
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
