fdd38de353eb90d0629eb60d8955025b389323a8
hoonjong <qkr7627@naver.com>
2020-12-11 16:44:50 +0900

    Revert "첫번째 커밋입니다. -2020.12.10 신한대식당 시작"
    This reverts commit 52bce7f89fdaa907b103c8f4305d3e99fd0ac80b.
    
    2020.12.11 오전 12:49
    액션바 안뜨는 문제 해결 : 원인 : 메인액티비에서 extends를 AppCompatActivity 를 안하고, Activity.를 하고 있었음. --> 무슨차이 인지 고민해야 함
    
    2020.12.11 오후 4:41
    1.개발자에게 문의하기 --> 이미지로 변경
    2.공유하기 --> 플레이스토어에 업로드 후 손봐야 함
    3.의뢰자님께서 식단 업로드하실 때 기능 --> Hide로 사용할 예정.  공유하기 버튼 5번 클릭 시 식단 업로드 버튼 활성화 혹은 인텐트로 체인지

--- app/src/main/java/com/jongdroid/shinhan_food/MainActivity.java
+++ app/src/main/java/com/jongdroid/shinhan_food/MainActivity.java
@@ -1,9 +1,15 @@
 package com.jongdroid.shinhan_food;
 
 
+import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.app.Activity;
+
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.DatePicker;
 
 import android.widget.Toast;
@@ -20,7 +26,15 @@ public class MainActivity extends Activity {
 
         mDate = findViewById(R.id.datePicker);
 
-
+        //액션바 설정하기//
+        //액션바 타이틀 변경하기
+        //getSupportActionBar().setTitle("ACTIONBAR");
+        //액션바 배경색 변경
+        //getSupportActionBar().setBackgroundDrawable(new ColorDrawable(0xFF339999));
+        //홈버튼 표시
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        //액션바 숨기기
+        //hideActionBar();
 
 
         mDate.init(mDate.getYear(), mDate.getMonth(), mDate.getDayOfMonth(),
@@ -40,8 +54,7 @@ public class MainActivity extends Activity {
 
                         //monthOfYear는 0값이 1월을 뜻하므로 1을 더해줌 나머지는 같다.
 
-                        Toast.makeText(MainActivity.this, "테스트입니다" ,Toast.LENGTH_LONG).show();
-
+                        Toast.makeText(MainActivity.this, "테스트입니다", Toast.LENGTH_LONG).show();
 
 
                     }
