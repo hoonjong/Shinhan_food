f1a36025bd419f234b04c6b7e1903e0284a111d0
hoonjong <qkr7627@naver.com>
2020-12-15 23:25:52 +0900

    Revert "첫번째 커밋입니다. -2020.12.10 신한대식당 시작"
    2020.12.11 오전 12:49
    액션바 안뜨는 문제 해결 : 원인 : 메인액티비에서 extends를 AppCompatActivity 를 안하고, Activity.를 하고 있었음. --> 무슨차이 인지 고민해야 함
    
    2020.12.11 오후 4:41
    1.개발자에게 문의하기 --> 이미지로 변경
    2.공유하기 --> 플레이스토어에 업로드 후 손봐야 함
    3.의뢰자님께서 식단 업로드하실 때 기능 --> Hide로 사용할 예정.  공유하기 버튼 5번 클릭 시 식단 업로드 버튼 활성화 혹은 인텐트로 체인지
    
    2020.12.11 오후 6:00
    1.3번 기능 --> 별모양 클릭시 인텐트로 화면 전환해서 메뉴 입력하는 창 까지 성공.  메뉴 입력하는 부분 구현해야 함
    
    2020.12.15 오후 11:25
    Test for Exam

--- app/src/main/java/com/jongdroid/shinhan_food/InputActivity.java
+++ app/src/main/java/com/jongdroid/shinhan_food/InputActivity.java
@@ -1,15 +1,67 @@
 package com.jongdroid.shinhan_food;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
 public class InputActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_input);
 
+        final EditText Text1;
+        DatePicker mDate;
+        Button btn1;
+        Text1 = findViewById(R.id.et1);
+        mDate = findViewById(R.id.datePicker);
+        btn1 = findViewById(R.id.btn1);
+
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(InputActivity.this, MainActivity.class);
+                intent.putExtra("Text1", Text1.getText().toString());// 이 메서드를 통해 데이터를 전달합니다.
+                startActivity(intent);
+            }
+        });
+
+
+        mDate.init(mDate.getYear(), mDate.getMonth(), mDate.getDayOfMonth(),
+
+                new DatePicker.OnDateChangedListener() {
+
+                    //값이 바뀔때마다 텍스트뷰의 값을 바꿔준다.
+
+                    @Override
+
+                    public void onDateChanged(DatePicker view, int year, int monthOfYear,
+
+                                              int dayOfMonth) {
+
+                        //monthOfYear는 0값이 1월을 뜻하므로 1을 더해줌 나머지는 같다.
+                        Intent intent = new Intent(InputActivity.this, MainActivity.class);
+                        intent.putExtra("Text1", Text1.getText().toString());// 이 메서드를 통해 데이터를 전달합니다.
+                        startActivity(intent);
+                        //Edittext Text 지우기
+                        Text1.setText(null);
+                    }
+                });
+
+
+        }
+
 
     }
-}
+
+
+
--- app/src/main/java/com/jongdroid/shinhan_food/MainActivity.java
+++ app/src/main/java/com/jongdroid/shinhan_food/MainActivity.java
@@ -14,6 +14,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 
+import android.widget.TextView;
 import android.widget.Toast;
 
 
@@ -25,8 +26,14 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         mDate = findViewById(R.id.datePicker);
+        View1 = findViewById(R.id.tv1);
+
+
+        Intent intent = getIntent();
+        String receiveStr = intent.getExtras().getString("Text1");// 전달한 값을 받을 때
+        View1 = findViewById(R.id.tv1);
+        View1.setText(receiveStr);
 
 
         //버튼 설정 및 이벤트 처리
@@ -63,19 +70,61 @@ public class MainActivity extends Activity {
 
         mDate.init(mDate.getYear(), mDate.getMonth(), mDate.getDayOfMonth(),
 
-                new DatePicker.OnDateChangedListener() {
+            new DatePicker.OnDateChangedListener() {
 
-                    //값이 바뀔때마다 텍스트뷰의 값을 바꿔준다.
+                //값이 바뀔때마다 텍스트뷰의 값을 바꿔준다.
 
-                    @Override
+                @Override
 
-                    public void onDateChanged(DatePicker view, int year, int monthOfYear,
+                public void onDateChanged(DatePicker view, int year, int monthOfYear,
 
-                                              int dayOfMonth) {
+                                          int dayOfMonth) {
 
-                        //monthOfYear는 0값이 1월을 뜻하므로 1을 더해줌 나머지는 같다.
-                        Toast.makeText(MainActivity.this, "테스트입니다", Toast.LENGTH_LONG).show();
-                    }
-                });
+                    //monthOfYear는 0값이 1월을 뜻하므로 1을 더해줌 나머지는 같다.
+                    //Toast.makeText(MainActivity.this, "테스트입니다", Toast.LENGTH_LONG).show();
+                    Intent intent = getIntent();
+                    String receiveStr = intent.getExtras().getString("Text1");// 전달한 값을 받을 때
+                    View1 = findViewById(R.id.tv1);
+                    View1.setText(receiveStr);
+                }
+            });
+
+    } //oncreate 끝
+
+    //액션버튼 메뉴 액션바에 집어 넣기
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
     }
+
+    //액션버튼을 클릭했을때의 동작
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        //or switch문도 이용가능^^
+        if (id == R.id.action_chat) {
+            Intent email = new Intent(Intent.ACTION_SEND);
+            email.setType("plain/text");
+            // email setting 배열로 해놔서 복수 발송 가능
+            String[] address = {"qkr7627@gmail.com"};
+            email.putExtra(Intent.EXTRA_EMAIL, address);
+            email.putExtra(Intent.EXTRA_SUBJECT, "개발자님 문의합니다.");
+            email.putExtra(Intent.EXTRA_TEXT, "내용\n");
+            startActivity(email);
+            return true;
+        }
+        if (id == R.id.action_share) {
+            Intent msg = new Intent(Intent.ACTION_SEND);
+            msg.addCategory(Intent.CATEGORY_DEFAULT);
+            msg.putExtra(Intent.EXTRA_TEXT, "https://play.google.com/store/apps/details?id=com.jongdroid.shinhan_food");
+            msg.putExtra(Intent.EXTRA_TITLE, "제목");
+            msg.setType("text/plain");
+            startActivity(Intent.createChooser(msg, "앱을 선택해 주세요"));
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    } //onOptionsItemSelected의 끝
 }
+
